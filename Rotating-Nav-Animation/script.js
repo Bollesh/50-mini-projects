defd02d1da9f17786702796592d7840b48708007
Bollesh <vallabh.360@gmail.com>
2023-12-30 14:37:52 +0530

    Changed title and changed nav items' speed

--- Rotating-Nav-Animation/script.js
+++ Rotating-Nav-Animation/script.js
@@ -8,10 +8,15 @@ openBtn.addEventListener("click", () =>{
     circle.classList.add("rotate");
     mainContainer.classList.add("show-nav");
     navigation.style.transform = "translateX(0)";
+    document.querySelector("nav ul li + li").style.transform = "translateX(0)";
+    document.querySelector("nav ul li + li + li").style.transform = "translateX(0)";
+
 });
 
 closeBtn.addEventListener("click", () => {
     circle.classList.remove("rotate");
     mainContainer.classList.remove("show-nav");
     navigation.style.transform = "translateX(-100px)"
+    document.querySelector("nav ul li + li").style.transform = "translateX(-10px)";
+    document.querySelector("nav ul li + li + li").style.transform = "translateX(-20px)";
 });
